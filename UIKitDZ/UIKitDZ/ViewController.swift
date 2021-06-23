8af8c99a449021e0fac93130ba55b133d9518c0a
klintukhov <gyrzov86@gmail.com>
2021-06-23 17:47:28 +0200

    init

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by Григорий Клинтухов on 23/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
